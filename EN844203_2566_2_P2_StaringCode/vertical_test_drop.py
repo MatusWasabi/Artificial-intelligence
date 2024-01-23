c902dda60fa176935d1d90d331ba1c0cb11b6798
Matus Yaowvasrisuwan <matus.y@kkumail.com>
2024-01-23 20:37:42 +0700

    Remove Self made board

--- EN844203_2566_2_P2_StaringCode/vertical_test_drop.py
+++
@@ -1,9 +0,0 @@
-from board import Board
-
-print("Board 1 test vertical")
-board = Board(6, 7)
-board.DrawBoard()
-for i in range(3):
-    board.DropPiece(0 ,"X")
-board.DropPiece(0, "O")
-board.DropPiece(0, "X")
